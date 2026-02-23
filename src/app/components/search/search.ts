1d32d36b7d9f697842ccfb6336863474309b1d97
GiannfrancoCrovetto <gcrovetto2021@fau.edu>
2026-02-23 14:42:01 -0500

    added code for the search component

--- src/app/components/search/search.ts
+++ src/app/components/search/search.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RecipeService } from '../../services/recipe';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './search.html',
-  styleUrl: './search.css',
+  styleUrl: './search.css'
 })
 export class Search {
+  query: string = '';
+  meals: any[] = [];
+  loading: boolean = false;
+  noResults: boolean = false;
 
+  constructor(private recipeService: RecipeService, private router: Router) {}
+
+  search() {
+    if (!this.query.trim()) return;
+    this.loading = true;
+    this.noResults = false;
+
+    this.recipeService.searchMeals(this.query).subscribe(results => {
+      this.meals = results;
+      this.noResults = results.length === 0;
+      this.loading = false;
+    });
+  }
+
+  goToDetail(id: string) {
+    this.router.navigate(['/recipe', id]);
+  }
 }
