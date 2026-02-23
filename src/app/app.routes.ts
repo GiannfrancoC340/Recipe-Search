f105edf32598b5046c64d1d8993fa3602173d42b
GiannfrancoCrovetto <gcrovetto2021@fau.edu>
2026-02-23 14:32:01 -0500

    added basic code and changed naming conventions

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Search } from './components/search/search';
+import { RecipeDetail } from './components/recipe-detail/recipe-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Search },
+  { path: 'recipe/:id', component: RecipeDetail },
+  { path: '**', redirectTo: '' }
+];
--- src/app/services/recipe.ts
+++ src/app/services/recipe.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Recipe {
-  
+export class RecipeService {
+  private baseUrl = 'https://www.themealdb.com/api/json/v1/1';
+
+  constructor(private http: HttpClient) {}
+
+  searchMeals(query: string): Observable<any[]> {
+    return this.http.get<any>(`${this.baseUrl}/search.php?s=${query}`).pipe(
+      map(response => response.meals || [])
+    );
+  }
+
+  getMealById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.baseUrl}/lookup.php?i=${id}`).pipe(
+      map(response => response.meals[0])
+    );
+  }
 }
